ab7d7480d5d9882b780f3cdff8ffcacf383f163a
RSLastra <raul.slastra@gmail.com>
2024-10-22 09:44:32 -0600

    feat: payment slider

--- src/components/PaymentMethods/PaymentMethods.tsx
+++ src/components/PaymentMethods/PaymentMethods.tsx
@@ -11,12 +11,26 @@ export function PaymentMethods() {
     <div className='relative py-20 md:py-64' id='servicios'>
       <BackgroundRadialRight />
       <div className='relative w-full overflow-hidden'>
-        <Swiper>
+        <Swiper
+          breakpoints={{
+            320: {
+              slidesPerView: 8,
+              spaceBetween: 15
+            }
+          }}
+          autoplay={{
+            delay: 1,
+            disableOnInteraction: false
+          }}
+          grabCursor={true}
+          loop={true}
+          speed={2000}
+          >
           <div className='absolute flex'>
             {
               dataPaymentMethods.map(({ id, image }) => (
                 <SwiperSlide key={id} className='flex items-center slider-horizontal'>
-                  <Image src={`/assets/${image}`} alt='metodo de pago' width={70} height={70} className='w-[120px] h-[60px] object-contain' />
+                  <Image src={`/assets/${image}`} alt='payment' width={70} height={70} className='w-[120px] h-[60px] object-contain' />
                 </SwiperSlide>
               ))
             }
